f37b6d99d00d8fdfb44094dbc202b2771bf863e5
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2021-11-30 17:15:17 -0600

    Quiz Project
    Up to Submit Buttons

---
+++ force-app/main/default/lwc/quizApp/quizApp.js
@@ -0,0 +1,62 @@
+import { LightningElement } from 'lwc';
+
+export default class QuizApp extends LightningElement {
+
+    selected={} //for storing answers
+    myQuestions=[
+        {
+            id: "Question1",
+            question: "Which one of the file is invalid in LWC component folder",
+            answers:{
+                a:".svg",
+                b:".apex",
+                c:".js"
+            },
+            correctAnswer:"b"
+        },
+        {
+            id: "Question2",
+            question: "Which one of the following is not a template loop",
+            answers:{
+                a:"for:each",
+                b:"iteration",
+                c:"map loop"
+            },
+            correctAnswer:"c"
+        },
+        {
+            id: "Question3",
+            question: "Which one of the following is not a directive",
+            answers:{
+                a:"for:each",
+                b:"if:true",
+                c:"map:loop"
+            },
+            correctAnswer:"c"
+        }
+
+    ]
+
+    get allNotSelected(){
+        return !(Object.keys(this.selected).length === this.myQuestions.length)
+    }
+    changeHandler(event){
+        console.log("name",event.target.name)
+        console.log("value",event.target.value)
+        //const name = event.target.name; 
+        //const value = event.target.value;
+        //can also write above two as shorthand like this- 
+        const {name,value} = event.target
+        this.selected={...this.selected,[name]:value}   //to store these values in selected object. 
+        //take whatever is selected
+        // inside the property and name is key and store the value along the name property
+        // in the backend -> this.selected={"Question 1":"a"} 
+    }
+    submitHandler(){
+
+    }
+
+    resetHandler(){
+
+    }
+}
